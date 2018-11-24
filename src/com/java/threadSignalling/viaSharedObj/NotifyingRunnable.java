5514fd873369f788db55771ccfe452872a5a3a79
Narendra Nirala <nknirala.007@gmail.com>
2018-11-24 19:54:24 +0530

    Wait notify.

---
+++ src/com/java/threadSignalling/viaSharedObj/NotifyingRunnable.java
@@ -0,0 +1,23 @@
+package com.java.threadSignalling.viaSharedObj;
+
+public class NotifyingRunnable implements Runnable {
+
+    private SharedSignal sharedSignal;
+
+    public NotifyingRunnable(SharedSignal sharedSignal) {
+        this.sharedSignal = sharedSignal;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("before notifying");
+        try{
+            Thread.sleep(5);
+        }catch(InterruptedException ex){
+
+        }
+        sharedSignal.setHasDataToProcess(true);
+        System.out.println("After Notifying");
+
+    }
+}
